512f93de5b0e8aba4a52a388542bc82607501510
BelChaparro <a.belenchaparro@gmail.com>
2024-02-09 19:59:04 -0300

    Added swagger route and tasks route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
+router.use("/", require("./swagger"));
+
 /**********************************************
  * Route to use for all User Profile Management
  **********************************************/
 router.use("/users", require("./userRoutes"));
+
+router.use("/tasks", require("./taskRoutes"));
 
 module.exports = router;

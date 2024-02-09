a50c41cfaa405852b8b84a3a8978c0890d46638d
BelChaparro <a.belenchaparro@gmail.com>
2024-02-09 19:59:26 -0300

    Commented out swagger headers console log

--- routes/swagger.js
+++ routes/swagger.js
@@ -3,12 +3,13 @@ const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("../swagger_output.json");
 
 // Middleware to log headers
-router.use("/api-docs", (req, res, next) => {
-    console.log("Headers in Swagger UI request:", req.headers);
-    next();
-});
+// router.use("/api-docs", (req, res, next) => {
+//     console.log("Headers in Swagger UI request:", req.headers);
+//     next();
+// });
 
 // Serve Swagger UI
+router.use("/api-docs", swaggerUi.serve);
 router.get("/api-docs", swaggerUi.setup(swaggerDocument));
 
 module.exports = router;

1308b36aeadd08935fd15df74c5a79515cf9b3bc
Cesar Coutino <ccoutino@gmail.com>
2024-02-12 13:20:18 -0700

    Clean the authentication and validation

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -3,27 +3,27 @@ const commentControllers = require("../controllers/commentControllers");
 const express = require("express");
 const router = express.Router();
 const { commentValidation } = require("../middleware/userValidation");
-const authenticate = require('../middleware/authenticate');
+const { isAuthenticated } = require('../middleware/authenticate');
 
 // Route to get all comments
-router.get("/", authenticate.isAuthenticated, commentControllers.getAllComments);
+router.get("/", isAuthenticated, commentControllers.getAllComments);
 
 // Route to get a comment by commentID
-router.get("/:id", authenticate.isAuthenticated, commentControllers.getCommentsById);
+router.get("/:id", isAuthenticated, commentControllers.getCommentsById);
 
 // Route to get a comment(s) by authorID
-router.get("/user/:id", authenticate.isAuthenticated, commentControllers.getCommentsByUserId);
+router.get("/user/:id", isAuthenticated, commentControllers.getCommentsByUserId);
 
 // Route to get a comment(s) by taskID
-router.get("/task/:id", authenticate.isAuthenticated, commentControllers.getCommentsByTaskId);
+router.get("/task/:id", isAuthenticated, commentControllers.getCommentsByTaskId);
 
 // Route to create a comment
-router.post("/", authenticate.isAuthenticated, commentValidation, commentControllers.createComment);
+router.post("/", isAuthenticated, commentValidation, commentControllers.createComment);
 
 // Route to update a comment by commentID
-router.put("/:id", authenticate.isAuthenticated, commentValidation, commentControllers.updateCommentById);
+router.put("/:id", isAuthenticated, commentValidation, commentControllers.updateCommentById);
 
 // Route to delete a comment by commentID
-router.delete("/:id", authenticate.isAuthenticated, commentControllers.deleteCommentById);
+router.delete("/:id", isAuthenticated, commentControllers.deleteCommentById);
 
 module.exports = router;

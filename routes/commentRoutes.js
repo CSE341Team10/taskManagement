165dc120aa572732b91337e0551877f476341112
Cesar Coutino <ccoutino@gmail.com>
2024-02-12 13:17:24 -0700

    Add authentication to routes

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -3,26 +3,27 @@ const commentControllers = require("../controllers/commentControllers");
 const express = require("express");
 const router = express.Router();
 const { commentValidation } = require("../middleware/userValidation");
+const authenticate = require('../middleware/authenticate');
 
 // Route to get all comments
-router.get("/", commentControllers.getAllComments);
+router.get("/", authenticate.isAuthenticated, commentControllers.getAllComments);
 
 // Route to get a comment by commentID
-router.get("/:id", commentControllers.getCommentsById);
+router.get("/:id", authenticate.isAuthenticated, commentControllers.getCommentsById);
 
 // Route to get a comment(s) by authorID
-router.get("/user/:id", commentControllers.getCommentsByUserId);
+router.get("/user/:id", authenticate.isAuthenticated, commentControllers.getCommentsByUserId);
 
 // Route to get a comment(s) by taskID
-router.get("/task/:id", commentControllers.getCommentsByTaskId);
+router.get("/task/:id", authenticate.isAuthenticated, commentControllers.getCommentsByTaskId);
 
 // Route to create a comment
-router.post("/", commentValidation, commentControllers.createComment);
+router.post("/", authenticate.isAuthenticated, commentValidation, commentControllers.createComment);
 
 // Route to update a comment by commentID
-router.put("/:id", commentValidation, commentControllers.updateCommentById);
+router.put("/:id", authenticate.isAuthenticated, commentValidation, commentControllers.updateCommentById);
 
 // Route to delete a comment by commentID
-router.delete("/:id", commentControllers.deleteCommentById);
+router.delete("/:id", authenticate.isAuthenticated, commentControllers.deleteCommentById);
 
 module.exports = router;

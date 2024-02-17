705496f7eee760604982f3d5050cf9cd2e683051
BelChaparro <a.belenchaparro@gmail.com>
2024-02-17 00:24:14 -0300

    added validation to ObjectId and removed possibility to change userId from tasks. userId is set with session user id

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -18,7 +18,13 @@ const getAllTasks = async (req, res) => {
 const getTaskById = async (req, res) => {
     //#swagger.tags=['Tasks']
     //#swagger.description='Gets a specific tasks by its ID.'
+
     try {
+        // Validate task ID:
+        if (!ObjectId.isValid(req.params.id)) {
+            res.status(400).json("You must use a valid task ID to find a task.");
+        }
+
         const taskId = new ObjectId(req.params.id);
         const task = await Task.find({ _id: taskId });
 
@@ -40,7 +46,12 @@ const getTasksByUserId = async (req, res) => {
     //#swagger.description='Gets a specific tasks by the user's ID.'
     
     try {
-        const userId = req.params.id;
+        // Validate user ID:
+        if (!ObjectId.isValid(req.params.id)) {
+            res.status(400).json("You must use a valid user ID to find a task by user ID.");
+        }
+
+        const userId = new ObjectId(req.params.id);
         const tasks = await Task.find({ userId: userId });
 
         if (tasks.length > 0) {
@@ -69,7 +80,7 @@ const createTask = async (req, res) => {
         dueDate: formatDate(req.body.dueDate),
         priorityLevel: req.body.priorityLevel,
         status: req.body.status,
-        userId: req.body.userId
+        userId: req.session.user._id // Sets userId with session id
       });
 
         await task.save();
@@ -84,18 +95,22 @@ const createTask = async (req, res) => {
 const updateTask = async (req, res) => {
     //#swagger.tags=['Tasks']
     //#swagger.description='Updates a specific taks by its ID.'
+    // Update is not available for userId:
     const task = {
         title: req.body.title,
         description: req.body.description,
         dueDate: formatDate(req.body.dueDate), 
         priorityLevel: req.body.priorityLevel,
         status: req.body.status,
-        userId: req.body.userId
     };
 
     try {
-        const taskId = new ObjectId(req.params.id);
-        
+        // Validate task ID:
+        if (!ObjectId.isValid(req.params.id)) {
+            res.status(400).json("You must use a valid task ID to update a task.");
+        }
+
+        const taskId = new ObjectId(req.params.id);        
         const taskUpdate = await Task.findOneAndUpdate({ _id: taskId}, task, {new: true});
         
         if (taskUpdate) {
@@ -114,8 +129,13 @@ const deleteTask = async (req, res) => {
     //#swagger.tags=['Tasks']
     //#swagger.description='Deletes a specific taks by its ID.'
     try {
+        // Validate task ID:
+        if (!ObjectId.isValid(req.params.id)) {
+            res.status(400).json(
+                "You must use a valid task ID to delete a task."
+            );
+        }
         const taskId = new ObjectId(req.params.id);
-
         const taskDelete = await Task.deleteOne({ _id: taskId});
 
         if (taskDelete.deletedCount > 0) {
